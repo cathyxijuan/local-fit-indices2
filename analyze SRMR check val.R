57f0722fbb4222e1adb853a9f3b02c1245c62eb1
Xijuan Zhang <cathyxijuan@gmail.com>
2023-05-25 10:14:39 -0400

    add results

--- analyze SRMR check val.R
+++ analyze SRMR check val.R
@@ -1,2 +1,18 @@
 source("functions.R")
 load("pop.indices.RData")
+load("srmr.ci.check.val.mod.orig.n150.RData")
+load("srmr.ci.check.val.mod.orig.n200.RData")
+load("srmr.ci.check.val.mod.orig.n300.RData")
+load("srmr.ci.check.val.mod.orig.n500.RData")
+load("srmr.ci.check.val.mod.orig.n800.RData")
+load("srmr.ci.check.val.mod.orig.n1000.RData")
+
+
+srmr.check.val.orig.mod <- list("n150"=list.mean(srmr.ci.check.val.mod.orig.n200),
+                                "n200"=list.mean(srmr.ci.check.val.mod.orig.n200),
+                                "n300"=list.mean(srmr.ci.check.val.mod.orig.n300),
+                                "n500"=list.mean(srmr.ci.check.val.mod.orig.n500),
+                                "n800"=list.mean(srmr.ci.check.val.mod.orig.n800),
+                                "n1000"=list.mean(srmr.ci.check.val.mod.orig.n1000))
+
+save(srmr.check.val.orig.mod, file="srmr.check.val.orig.mod.RData")
