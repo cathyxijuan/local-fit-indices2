fb7afc00d52241782a6013a929cbb00c9048a82d
Xijuan Zhang <cathyxijuan@gmail.com>
2023-05-28 17:20:11 -0400

    add

--- analyze SRMR check val.R
+++ analyze SRMR check val.R
@@ -15,4 +15,6 @@ srmr.check.val.orig.mod <- list("n150"=list.mean(srmr.ci.check.val.mod.orig.n200
                                 "n800"=list.mean(srmr.ci.check.val.mod.orig.n800),
                                 "n1000"=list.mean(srmr.ci.check.val.mod.orig.n1000))
 
-save(srmr.check.val.orig.mod, file="srmr.check.val.orig.mod.RData")
+save(srmr.check.val.orig.mod, 
+     file="srmr.check.val.orig.mod.RData")
+
